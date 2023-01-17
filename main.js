1d4ebd0ce89ec1d3eea6deeebe5e96a33ac72742
SayDeivis <marchandeivis@gmail.com>
2023-01-17 19:25:19 -0400

    Se agrega el proyecto

---
+++ main.js
@@ -0,0 +1,25 @@
+document.querySelector('.menu-btn').addEventListener('click',()=>{
+   document.querySelector('.nav-menu-2').classList.toggle('show');
+})
+
+ScrollReveal().reveal('.showcase', {delay:500});
+ScrollReveal().reveal('.new-card', {delay:500});
+ScrollReveal().reveal('.new-banner', {delay:500});
+ScrollReveal().reveal('.new-banner-2', {delay:500});
+ScrollReveal().reveal('.social', {delay:500});
+ScrollReveal().reveal('footer', {delay:500});
+ScrollReveal().reveal('.copyright', {delay:500});
+
+
+
+
+
+
+/* const btn = document.querySelector('.menu-btn');
+console.log(btn);
+const navMenu = document.querySelector('.nav-menu-2')
+console.log(navMenu);
+
+btn.addEventListener('click',()=>{
+   btn.classList.toggle('show');
+}) */
